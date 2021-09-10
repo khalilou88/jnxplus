7ae549c121077b179cb34ff50b28b0df941ad39d
khalilou <khalil.lagrida@gmail.com>
2021-09-10 03:57:42 +0200

    initial commit

---
+++ packages/nx-boot-gradle/src/generators/nx-boot-gradle/generator.spec.ts
@@ -0,0 +1,20 @@
+import { createTreeWithEmptyWorkspace } from '@nrwl/devkit/testing';
+import { Tree, readProjectConfiguration } from '@nrwl/devkit';
+
+import generator from './generator';
+import { NxBootGradleGeneratorSchema } from './schema';
+
+describe('nx-boot-gradle generator', () => {
+  let appTree: Tree;
+  const options: NxBootGradleGeneratorSchema = { name: 'test' };
+
+  beforeEach(() => {
+    appTree = createTreeWithEmptyWorkspace();
+  });
+
+  it('should run successfully', async () => {
+    await generator(appTree, options);
+    const config = readProjectConfiguration(appTree, 'test');
+    expect(config).toBeDefined();
+  });
+});
---
+++ packages/nx-boot-gradle/src/generators/nx-boot-gradle/schema.d.ts
@@ -0,0 +1,5 @@
+export interface NxBootGradleGeneratorSchema {
+  name: string;
+  tags?: string;
+  directory?: string;
+}
