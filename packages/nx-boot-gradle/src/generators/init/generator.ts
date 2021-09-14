0399a3164522c9509d4e4812bda873d0cbb416da
kl <khalil.lagrida@gmail.com>
2021-09-14 13:52:30 +0200

    change permission for gradlew

--- packages/nx-boot-gradle/src/generators/init/generator.ts
+++ packages/nx-boot-gradle/src/generators/init/generator.ts
@@ -36,6 +36,7 @@ export default async function (
   addFiles(tree, normalizedOptions);
   updateNxJson(tree);
   updateGitIgnoreFile(tree);
+  tree.changePermissions('gradlew', 755);
   await formatFiles(tree);
 }
 
