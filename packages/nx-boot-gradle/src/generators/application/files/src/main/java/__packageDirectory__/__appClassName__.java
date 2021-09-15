43f1ee2b61258a07bec3861ebb6ab300cedf5804
kl <khalil.lagrida@gmail.com>
2021-09-15 15:48:04 +0200

    add scanBasePackages to app

--- packages/nx-boot-gradle/src/generators/application/files/src/main/java/__packageDirectory__/__appClassName__.java
+++ packages/nx-boot-gradle/src/generators/application/files/src/main/java/__packageDirectory__/__appClassName__.java
@@ -3,7 +3,7 @@ package <%= packageName %>;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "<%= groupId %>")
 public class <%= appClassName %> {
 
   public static void main(String[] args) {
