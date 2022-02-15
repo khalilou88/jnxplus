63351893424b3a2369f5b60d5a1d713d060b5b8a
kl <khalil.lagrida@gmail.com>
2022-02-15 13:42:18 +0100

    update spring boot version to 2.6.3

--- packages/nx-boot-gradle/src/utils/versions.ts
+++ packages/nx-boot-gradle/src/utils/versions.ts
@@ -1,4 +1,4 @@
-export const springBootVersion = '2.6.2';
+export const springBootVersion = '2.6.3';
 export const dependencyManagementVersion = '1.0.11.RELEASE';
 export const kotlinJvmVersion = '1.6.10';
 export const kotlinSpringVersion = '1.6.10';
--- packages/nx-boot-maven/src/utils/versions.ts
+++ packages/nx-boot-maven/src/utils/versions.ts
@@ -1,2 +1,2 @@
-export const springBootStarterParentVersion = '2.6.2';
+export const springBootStarterParentVersion = '2.6.3';
 export const kotlinVersion = '1.6.10';
