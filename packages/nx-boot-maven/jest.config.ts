540632104e452df878577ff3b344edd374c52671
kl <khalil.lagrida@gmail.com>
2022-04-22 11:50:15 +0200

    upgrade to Nx 14

--- packages/nx-boot-maven/jest.config.ts
+++ packages/nx-boot-maven/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
-  displayName: 'nx-boot-gradle',
-  preset: '../../jest.preset.js',
+  displayName: 'nx-boot-maven',
+
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -11,5 +11,6 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/nx-boot-gradle',
+  coverageDirectory: '../../coverage/packages/nx-boot-maven',
+  preset: '..\\..\\jest.preset.ts',
 };
