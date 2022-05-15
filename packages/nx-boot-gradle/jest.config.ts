6e8b193d73cb2065b77d41e9d2ea2177417db827
kl <khalil.lagrida@gmail.com>
2022-05-15 11:19:59 +0200

    update Nx

--- packages/nx-boot-gradle/jest.config.ts
+++ packages/nx-boot-gradle/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: 'nx-boot-gradle',
 
   globals: {
@@ -12,5 +12,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/nx-boot-gradle',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
 };
