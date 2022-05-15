e59b09155bccfe01a6a164bec48620420936f6c7
kl <khalil.lagrida@gmail.com>
2022-05-15 11:28:40 +0200

    change file extension

--- e2e/nx-boot-gradle-e2e/jest.config.js
+++ e2e/nx-boot-gradle-e2e/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   displayName: 'nx-boot-gradle-e2e',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
