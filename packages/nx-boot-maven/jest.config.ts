fef3804e1eda51c30aa94ea458fac31c48bd65cf
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2022-05-15 12:04:46 +0200

    Feature/update nx 14 1 5 (#38)
    * update Nx
    
    * update Nx
    
    * remove @jscutlery/semver
    
    * change file extension

--- packages/nx-boot-maven/jest.config.ts
+++ packages/nx-boot-maven/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: 'nx-boot-maven',
 
   globals: {
@@ -12,5 +12,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/packages/nx-boot-maven',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
 };
