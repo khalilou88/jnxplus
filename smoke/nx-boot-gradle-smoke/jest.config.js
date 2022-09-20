931ea3c5e83c338b5baa3d3ffb927a54337909ef
kl <khalil.lagrida@gmail.com>
2022-09-20 14:46:59 +0200

    add smoke tests

---
+++ smoke/nx-boot-gradle-smoke/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'nx-boot-gradle-smoke',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/smoke/nx-boot-gradle-smoke',
+};
