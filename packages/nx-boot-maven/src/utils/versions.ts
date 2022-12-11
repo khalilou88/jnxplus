4be988796502a1d1b4193c3632329b5572fbbff6
kl <khalil.lagrida@gmail.com>
2022-12-11 16:25:06 +0100

    Spring Boot 3

--- packages/nx-boot-maven/src/utils/versions.ts
+++ packages/nx-boot-maven/src/utils/versions.ts
@@ -1,2 +1,2 @@
-export const springBootStarterParentVersion = '2.7.0';
-export const kotlinVersion = '1.6.21';
+export const springBootStarterParentVersion = '3.0.0';
+export const kotlinVersion = '1.7.21';
