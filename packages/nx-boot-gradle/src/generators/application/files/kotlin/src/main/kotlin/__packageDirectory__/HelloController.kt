99edf1085be3fc9d5b34e634678b6f6477d31420
kl <khalil.lagrida@gmail.com>
2022-12-23 20:01:38 +0100

    fix generated kotlin tests

--- packages/nx-boot-gradle/src/generators/application/files/kotlin/src/main/kotlin/__packageDirectory__/HelloController.kt
+++ packages/nx-boot-gradle/src/generators/application/files/kotlin/src/main/kotlin/__packageDirectory__/HelloController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloController {
 
-    @GetMapping
+    @GetMapping("/")
     fun greeting():String = "Hello World!"
 
 }
