b28df5594d5a8a523ed6216f99ebf8a5d482d3b0
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2022-12-24 22:41:13 +0100

    update to Spring Boot 3.0.1 (#108)
    * update to Spring Boot 3.0.1
    
    * update gradle wrapper

--- packages/nx-boot-maven/src/utils/versions.ts
+++ packages/nx-boot-maven/src/utils/versions.ts
@@ -1,2 +1,2 @@
-export const springBootStarterParentVersion = '3.0.0';
-export const kotlinVersion = '1.7.21';
+export const springBootStarterParentVersion = '3.0.1';
+export const kotlinVersion = '1.7.22';
