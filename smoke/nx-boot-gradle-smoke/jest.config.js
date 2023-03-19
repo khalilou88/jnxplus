49a4941165f4c7d95bab5fc20825093d29807d31
kl <khalil.lagrida@gmail.com>
2023-03-19 12:12:57 +0100

    update nx version to 15.8.7

--- smoke/nx-boot-gradle-smoke/jest.config.js
+++ smoke/nx-boot-gradle-smoke/jest.config.js
@@ -1,13 +1,14 @@
 module.exports = {
   displayName: 'nx-boot-gradle-smoke',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/smoke/nx-boot-gradle-smoke',
