508a3e00d2b4337a309821f14452f4fec68af3da
Clémentine PERREAUT <104975441+cperreaut@users.noreply.github.com>
2023-04-04 20:31:04 +0200

    Add exports of all the nx-boot-maven generators (#116)
    * Add exports of all the nx-boot-maven generators
    
    Add exports of all the nx-boot-maven generators to be able to import and extend them in other projects.
    
    * fix typo in filename
    
    * repair PR e2e tests by correcting  project.json exports

---
+++ packages/nx-boot-maven/generators.ts
@@ -0,0 +1,4 @@
+export * from './src/generators/application/generator';
+export * from './src/generators/init/generator';
+export * from './src/generators/library/generator';
+export * from './src/generators/migrate/generator';
