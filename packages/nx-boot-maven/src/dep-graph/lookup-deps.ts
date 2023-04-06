d4609f6d3119156dd5c07db561c005f9ae2f17a5
kl <khalil.lagrida@gmail.com>
2023-04-06 16:14:13 +0200

    calculate dep graph for all maven projects

--- packages/nx-boot-maven/src/dep-graph/lookup-deps.ts
+++ packages/nx-boot-maven/src/dep-graph/lookup-deps.ts
@@ -23,36 +23,31 @@ export function processProjectGraph(
   for (const project of projects) {
     const pomXmlPath = join(workspaceRoot, project.data.root, 'pom.xml');
 
-    if (fileExists(pomXmlPath)) {
-      const pomXmlContent = readXml(pomXmlPath);
-      const dependencies = getDependencies(pomXmlContent, projectNames);
-      for (const dependency of dependencies) {
-        builder.addStaticDependency(
-          project.name,
-          dependency,
-          join(project.data.root, 'pom.xml').replace(/\\/g, '/')
-        );
-      }
+    const pomXmlContent = readXml(pomXmlPath);
+    const dependencies = getDependencies(pomXmlContent, projectNames);
+    for (const dependency of dependencies) {
+      builder.addStaticDependency(
+        project.name,
+        dependency,
+        join(project.data.root, 'pom.xml').replace(/\\/g, '/')
+      );
     }
   }
 
   return builder.getUpdatedProjectGraph();
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function getManagedProjects(nodes: Record<string, ProjectGraphProjectNode>) {
   return Object.entries(nodes)
     .filter((node) => isManagedProject(node[1]))
     .map((node) => node[1]);
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function isManagedProject(projectGraphNode: ProjectGraphProjectNode): boolean {
+  const pomXmlPath = join(workspaceRoot, projectGraphNode.data.root, 'pom.xml');
   return (
     (projectGraphNode.type === 'app' || projectGraphNode.type === 'lib') &&
-    projectGraphNode.data?.targets?.build?.executor?.includes(
-      '@jnxplus/nx-boot-maven'
-    )
+    fileExists(pomXmlPath)
   );
 }
 
