772c35d920a9040fb5f7af60a66b0bb141ab3238
kl <khalil.lagrida@gmail.com>
2023-04-09 18:42:07 +0200

    remove used rule

--- packages/nx-boot-maven/src/dep-graph/lookup-deps.ts
+++ packages/nx-boot-maven/src/dep-graph/lookup-deps.ts
@@ -13,7 +13,6 @@ import { readXml } from '../utils/xml';
 
 export function processProjectGraph(
   graph: ProjectGraph,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   context: ProjectGraphProcessorContext
 ): ProjectGraph {
   const builder = new ProjectGraphBuilder(graph);
