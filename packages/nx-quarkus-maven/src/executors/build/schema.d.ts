a0561314001a7f4cf60785a4d70b50c879ef107d
kl <khalil.lagrida@gmail.com>
2023-04-13 00:52:30 +0200

    work in progress

--- packages/nx-quarkus-maven/src/executors/build/schema.d.ts
+++ packages/nx-quarkus-maven/src/executors/build/schema.d.ts
@@ -1,3 +1,7 @@
+import { BuildModeType, PackagingType } from '../../utils/types';
+
 export interface BuildExecutorSchema {
+  buildMode: BuildModeType;
+  packaging: PackagingType;
   mvnArgs: string;
 }
--- packages/nx-quarkus-maven/src/utils/types.ts
+++ packages/nx-quarkus-maven/src/utils/types.ts
@@ -1,4 +1,5 @@
 export type LanguageType = 'java' | 'kotlin';
-export type PackagingType = 'jar' | 'war';
+export type PackagingType = 'jar' | 'war' | 'native';
+export type BuildModeType = 'compile' | 'package' | 'install';
 export type LinterType = 'checkstyle' | 'pmd' | 'ktlint';
 export type PackageNameType = 'short' | 'long';
