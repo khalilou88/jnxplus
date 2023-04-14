20bc2af70d2dc999399f491f7941c3991828737b
kl <khalil.lagrida@gmail.com>
2023-04-14 18:19:58 +0200

    work in progress

---
+++ packages/nx-quarkus-maven/src/executors/build-image/executor.ts
@@ -0,0 +1,30 @@
+import { ExecutorContext, logger } from '@nrwl/devkit';
+import { getProjectSourceRoot, runCommand } from '../../utils/command';
+import { BuildImageExecutorSchema } from './schema';
+
+export default async function runExecutor(
+  options: BuildImageExecutorSchema,
+  context: ExecutorContext
+) {
+  logger.info(`Executor ran for Build Image: ${JSON.stringify(options)}`);
+
+  let imageNameSuffix = '';
+
+  if (options.imageNameSuffix) {
+    imageNameSuffix = `-${options.imageNameSuffix}`;
+  } else {
+    if (options.dockerfile === 'jvm') {
+      imageNameSuffix = `-jvm`;
+    }
+
+    if (options.dockerfile === 'legacy-jar') {
+      imageNameSuffix = `-legacy-jar`;
+    }
+  }
+
+  const projectSourceRoot = getProjectSourceRoot(context);
+
+  return runCommand(
+    `docker build -f ${projectSourceRoot}/main/docker/Dockerfile.${options.dockerfile} -t ${options.imageNamePrefix}/${context.projectName}${imageNameSuffix}`
+  );
+}
