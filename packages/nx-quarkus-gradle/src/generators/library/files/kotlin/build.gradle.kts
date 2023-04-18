ae89e687886ac6722be18b8beb5cacdc4d6d8be4
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-04-18 17:32:39 +0200

    work in progress

--- packages/nx-quarkus-gradle/src/generators/library/files/kotlin/build.gradle.kts
+++ packages/nx-quarkus-gradle/src/generators/library/files/kotlin/build.gradle.kts
@@ -1,5 +1,7 @@
 val javaVersion: String by project
-
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
 
 plugins {
     kotlin("jvm")
@@ -12,19 +14,12 @@ repositories {
     mavenLocal()
 }
 
-val quarkusPlatformGroupId: String by project
-val quarkusPlatformArtifactId: String by project
-val quarkusPlatformVersion: String by project
 
 dependencies {
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-kotlin")
-    implementation("io.quarkus:quarkus-resteasy-reactive-jackson")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.quarkus:quarkus-arc")
-    implementation("io.quarkus:quarkus-resteasy-reactive")
     testImplementation("io.quarkus:quarkus-junit5")
-    testImplementation("io.rest-assured:rest-assured")
 }
 
 group = "<%= groupId %>"
