e03238ecfca7372c652ccbc86bed67b0714a6b48
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-04-18 19:11:32 +0200

    work in progress

--- packages/nx-quarkus-gradle/src/generators/application/files/kotlin/build.gradle.kts
+++ packages/nx-quarkus-gradle/src/generators/application/files/kotlin/build.gradle.kts
@@ -1,5 +1,7 @@
 val javaVersion: String by project
-
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
 
 plugins {
     kotlin("jvm")
@@ -12,15 +14,9 @@ repositories {
     mavenLocal()
 }
 
-val quarkusPlatformGroupId: String by project
-val quarkusPlatformArtifactId: String by project
-val quarkusPlatformVersion: String by project
-
 dependencies {
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-kotlin")
-    implementation("io.quarkus:quarkus-resteasy-reactive-jackson")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-resteasy-reactive")
     testImplementation("io.quarkus:quarkus-junit5")
@@ -38,6 +34,7 @@ java {
 tasks.withType<Test> {
     systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
 }
+
 allOpen {
     annotation("javax.ws.rs.Path")
     annotation("javax.enterprise.context.ApplicationScoped")
--- packages/nx-quarkus-gradle/src/generators/library/files/kotlin/build.gradle.kts
+++ packages/nx-quarkus-gradle/src/generators/library/files/kotlin/build.gradle.kts
@@ -33,8 +33,8 @@ java {
 tasks.withType<Test> {
     systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
 }
+
 allOpen {
-    annotation("javax.ws.rs.Path")
     annotation("javax.enterprise.context.ApplicationScoped")
     annotation("io.quarkus.test.junit.QuarkusTest")
 }
