cd9d3c9ad345f9a9539b00957a0d4d9c63e89919
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-04-21 04:54:26 +0200

    work in progress

--- packages/nx-quarkus-maven/src/executors/ktformat/executor.ts
+++ packages/nx-quarkus-maven/src/executors/ktformat/executor.ts
@@ -1,14 +1,10 @@
-import { ExecutorContext, logger, workspaceRoot } from '@nrwl/devkit';
-import * as fs from 'fs';
-import * as path from 'path';
+import { ExecutorContext, logger } from '@nrwl/devkit';
 import {
-  downloadFile,
+  getKtlintAbsolutePath,
   getProjectSourceRoot,
   runCommand,
 } from '../../utils/command';
 import { KotlinFormatExecutorSchema } from './schema';
-
-import { ktlintVersion } from '../../utils/versions';
 
 export default async function runExecutor(
   options: KotlinFormatExecutorSchema,
@@ -17,24 +13,7 @@ export default async function runExecutor(
   logger.info(`Executor ran for Kotlin Format: ${JSON.stringify(options)}`);
   const projectSourceRoot = getProjectSourceRoot(context);
 
-  const url = `https://github.com/pinterest/ktlint/releases/download/${ktlintVersion}/ktlint`;
-
-  const outputDirectory = path.join(
-    workspaceRoot,
-    'node_modules',
-    '@jnxplus',
-    'tools',
-    'linters',
-    'ktlint'
-  );
-
-  if (!fs.existsSync(outputDirectory)) {
-    fs.mkdirSync(outputDirectory, { recursive: true });
-  }
-
-  const ktlintAbsolutePath = path.join(outputDirectory, 'ktlint');
-
-  await downloadFile(url, ktlintAbsolutePath);
+  const ktlintAbsolutePath = await getKtlintAbsolutePath();
 
   const command = `java --add-opens java.base/java.lang=ALL-UNNAMED -jar ${ktlintAbsolutePath} -F "${projectSourceRoot}/**/*.kt"`;
 
--- packages/nx-quarkus-maven/src/executors/lint/executor.ts
+++ packages/nx-quarkus-maven/src/executors/lint/executor.ts
@@ -1,14 +1,12 @@
-import { ExecutorContext, logger, workspaceRoot } from '@nrwl/devkit';
-import * as fs from 'fs';
-import * as path from 'path';
+import { ExecutorContext, logger } from '@nrwl/devkit';
 import {
-  downloadFile,
+  getKtlintAbsolutePath,
+  getPmdExecutable,
   getProjectSourceRoot,
+  getCheckstyleJarAbsolutePath,
   runCommand,
 } from '../../utils/command';
 import { LintExecutorSchema } from './schema';
-
-import { checkstyleVersion, ktlintVersion } from '../../utils/versions';
 
 export default async function runExecutor(
   options: LintExecutorSchema,
@@ -19,30 +17,7 @@ export default async function runExecutor(
   const projectSourceRoot = getProjectSourceRoot(context);
 
   if (options.linter === 'checkstyle') {
-    //TODO get checkstyleVersion from root parent pom
-    const checkstyleJarName = `checkstyle-${checkstyleVersion}-all.jar`;
-    const downloadUrl = `https://github.com/checkstyle/checkstyle/releases/download/checkstyle-${checkstyleVersion}/${checkstyleJarName}`;
-
-    const outputDirectory = path.join(
-      workspaceRoot,
-      'node_modules',
-      '@jnxplus',
-      'tools',
-      'linters',
-      'checkstyle'
-    );
-
-    if (!fs.existsSync(outputDirectory)) {
-      fs.mkdirSync(outputDirectory, { recursive: true });
-    }
-
-    const checkstyleJarAbsolutePath = path.join(
-      outputDirectory,
-      checkstyleJarName
-    );
-
-    await downloadFile(downloadUrl, checkstyleJarAbsolutePath);
-
+    const checkstyleJarAbsolutePath = await getCheckstyleJarAbsolutePath();
     command = `java -jar ${checkstyleJarAbsolutePath} -c ./tools/linters/checkstyle.xml ${projectSourceRoot}`;
   }
 
@@ -51,34 +26,9 @@ export default async function runExecutor(
   }
 
   if (options.linter === 'ktlint') {
-    //TODO get ktlint version from root parent pom
-
-    const url = `https://github.com/pinterest/ktlint/releases/download/${ktlintVersion}/ktlint`;
-
-    const outputDirectory = path.join(
-      workspaceRoot,
-      'node_modules',
-      '@jnxplus',
-      'tools',
-      'linters',
-      'ktlint'
-    );
-
-    if (!fs.existsSync(outputDirectory)) {
-      fs.mkdirSync(outputDirectory, { recursive: true });
-    }
-
-    const ktlintAbsolutePath = path.join(outputDirectory, 'ktlint');
-
-    await downloadFile(url, ktlintAbsolutePath);
-
+    const ktlintAbsolutePath = await getKtlintAbsolutePath();
     command = `java --add-opens java.base/java.lang=ALL-UNNAMED -jar ${ktlintAbsolutePath} "${projectSourceRoot}/**/*.kt"`;
   }
 
   return runCommand(command);
-}
-
-function getPmdExecutable() {
-  const isWin = process.platform === 'win32';
-  return isWin ? 'pmd.bat' : 'pmd';
 }
