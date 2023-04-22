52791ee525074f89d4fcd4a68cdbb241ea726214
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-04-22 18:14:42 +0200

    add simplePackageName

--- packages/nx-boot-maven/src/generators/library/schema.d.ts
+++ packages/nx-boot-maven/src/generators/library/schema.d.ts
@@ -5,6 +5,7 @@ export interface NxBootMavenLibGeneratorSchema {
   tags?: string;
   directory?: string;
   simpleName?: boolean;
+  simplePackageName?: boolean;
   language: LanguageType;
   groupId: string;
   projectVersion: string;
