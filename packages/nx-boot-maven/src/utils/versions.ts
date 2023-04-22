f625f3cf9008027a71ed5f0f695b44b45d5ce6b8
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-04-22 14:02:30 +0200

    update deps

--- packages/nx-boot-maven/src/utils/versions.ts
+++ packages/nx-boot-maven/src/utils/versions.ts
@@ -1,4 +1,4 @@
-export const springBootStarterParentVersion = '3.0.1';
+export const springBootStarterParentVersion = '3.0.6';
 export const kotlinVersion = '1.7.22';
 export const checkstyleVersion = '10.9.3';
 export const ktlintVersion = '0.48.2';
