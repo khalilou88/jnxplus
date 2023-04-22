48f444f04d9a6d411e15050f5a9f908cee28f208
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-04-22 11:34:10 +0200

    more checks

--- e2e/nx-boot-maven-e2e/tests/nx-boot-maven.spec.ts
+++ e2e/nx-boot-maven-e2e/tests/nx-boot-maven.spec.ts
@@ -27,6 +27,7 @@ describe('nx-boot-maven e2e', () => {
     process.env.CI === 'true' || process.env.GITHUB_ACTIONS === 'true';
   const isWin = process.platform === 'win32';
   const isMacOs = process.platform === 'darwin';
+  const parentProjectName = uniq('parent-project-');
   beforeAll(async () => {
     fse.ensureDirSync(tmpProjPath());
     cleanup();
@@ -46,7 +47,6 @@ describe('nx-boot-maven e2e', () => {
     );
     runPackageManagerInstall();
 
-    const parentProjectName = uniq('parent-project-');
     await runNxCommandAsync(
       `generate @jnxplus/nx-boot-maven:init --parentProjectName ${parentProjectName}`
     );
@@ -153,6 +153,20 @@ describe('nx-boot-maven e2e', () => {
     );
     expect(formatResult.stdout).toContain('');
 
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[appName]).toContainEqual({
+      type: 'static',
+      source: appName,
+      target: parentProjectName,
+    });
+
     const process = await runNxCommandUntil(`serve ${appName}`, (output) =>
       output.includes(`Tomcat started on port(s): 8080`)
     );
@@ -237,6 +251,20 @@ describe('nx-boot-maven e2e', () => {
     );
     expect(formatResult.stdout).toContain('');
 
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[appName]).toContainEqual({
+      type: 'static',
+      source: appName,
+      target: parentProjectName,
+    });
+
     const process = await runNxCommandUntil(
       `serve ${appName} --args="-Dspring-boot.run.profiles=test"`,
       (output) => output.includes(`Tomcat started on port(s): 8080`)
@@ -312,6 +340,20 @@ describe('nx-boot-maven e2e', () => {
     const lintResult = await runNxCommandAsync(`lint ${appName}`);
     expect(lintResult.stdout).toContain('Executor ran for Lint');
 
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[appName]).toContainEqual({
+      type: 'static',
+      source: appName,
+      target: parentProjectName,
+    });
+
     const process = await runNxCommandUntil(`serve ${appName}`, (output) =>
       output.includes(`Tomcat started on port(s): 8080`)
     );
@@ -378,6 +420,20 @@ describe('nx-boot-maven e2e', () => {
       `format:check --projects ${appName}`
     );
     expect(formatResult.stdout).toContain('');
+
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[appName]).toContainEqual({
+      type: 'static',
+      source: appName,
+      target: parentProjectName,
+    });
 
     const process = await runNxCommandUntil(
       `serve ${appName} --args="-Dspring-boot.run.profiles=test"`,
@@ -447,6 +503,20 @@ describe('nx-boot-maven e2e', () => {
     );
     expect(formatResult.stdout).toContain('');
 
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[appName]).toContainEqual({
+      type: 'static',
+      source: appName,
+      target: parentProjectName,
+    });
+
     const process = await runNxCommandUntil(
       `serve ${appName} --args="-Dspring-boot.run.profiles=test"`,
       (output) => output.includes(`Tomcat started on port(s): 8080`)
@@ -467,6 +537,20 @@ describe('nx-boot-maven e2e', () => {
     await runNxCommandAsync(
       `generate @jnxplus/nx-boot-maven:application ${appName} --directory deep/sub-dir`
     );
+
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[appName]).toContainEqual({
+      type: 'static',
+      source: appName,
+      target: parentProjectName,
+    });
 
     const process = await runNxCommandUntil(
       `serve deep-sub-dir-${appName}`,
@@ -530,6 +614,20 @@ describe('nx-boot-maven e2e', () => {
       `format:check --projects ${libName}`
     );
     expect(formatResult.stdout).toContain('');
+
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[libName]).toContainEqual({
+      type: 'static',
+      source: libName,
+      target: parentProjectName,
+    });
   }, 1200000);
 
   it('should create a kotlin library', async () => {
@@ -578,6 +676,20 @@ describe('nx-boot-maven e2e', () => {
 
     const lintResult = await runNxCommandAsync(`lint ${libName}`);
     expect(lintResult.stdout).toContain('Executor ran for Lint');
+
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[libName]).toContainEqual({
+      type: 'static',
+      source: libName,
+      target: parentProjectName,
+    });
   }, 1200000);
 
   it('should use the the specified properties to create a library', async () => {
@@ -626,6 +738,20 @@ describe('nx-boot-maven e2e', () => {
       `format:check --projects ${libName}`
     );
     expect(formatResult.stdout).toContain('');
+
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[libName]).toContainEqual({
+      type: 'static',
+      source: libName,
+      target: parentProjectName,
+    });
   }, 1200000);
 
   it('should generare a lib with a simple package name', async () => {
@@ -674,6 +800,20 @@ describe('nx-boot-maven e2e', () => {
       `format:check --projects ${libName}`
     );
     expect(formatResult.stdout).toContain('');
+
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[libName]).toContainEqual({
+      type: 'static',
+      source: libName,
+      target: parentProjectName,
+    });
   }, 1200000);
 
   it('--a lib with aliases', async () => {
@@ -722,6 +862,20 @@ describe('nx-boot-maven e2e', () => {
       `format:check --projects ${libName}`
     );
     expect(formatResult.stdout).toContain('');
+
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[libName]).toContainEqual({
+      type: 'static',
+      source: libName,
+      target: parentProjectName,
+    });
   }, 1200000);
 
   it('should add a lib to an app dependencies', async () => {
@@ -781,6 +935,18 @@ describe('nx-boot-maven e2e', () => {
     const depGraphJson = readJson('dep-graph.json');
     expect(depGraphJson.graph.nodes[appName]).toBeDefined();
     expect(depGraphJson.graph.nodes[libName]).toBeDefined();
+
+    expect(depGraphJson.graph.dependencies[appName]).toContainEqual({
+      type: 'static',
+      source: appName,
+      target: parentProjectName,
+    });
+
+    expect(depGraphJson.graph.dependencies[libName]).toContainEqual({
+      type: 'static',
+      source: libName,
+      target: parentProjectName,
+    });
 
     expect(depGraphJson.graph.dependencies[appName]).toContainEqual({
       type: 'static',
@@ -848,6 +1014,18 @@ describe('nx-boot-maven e2e', () => {
     expect(depGraphJson.graph.dependencies[appName]).toContainEqual({
       type: 'static',
       source: appName,
+      target: parentProjectName,
+    });
+
+    expect(depGraphJson.graph.dependencies[libName]).toContainEqual({
+      type: 'static',
+      source: libName,
+      target: parentProjectName,
+    });
+
+    expect(depGraphJson.graph.dependencies[appName]).toContainEqual({
+      type: 'static',
+      source: appName,
       target: libName,
     });
   }, 1200000);
@@ -871,6 +1049,13 @@ describe('nx-boot-maven e2e', () => {
     expect(depGraphResult.stderr).not.toContain(
       'Failed to process the project graph'
     );
+
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[libName]).toContainEqual({
+      type: 'static',
+      source: libName,
+      target: parentProjectName,
+    });
   }, 1200000);
 
   it('should generate java apps that use a parent project', async () => {
@@ -902,7 +1087,7 @@ describe('nx-boot-maven e2e', () => {
 
     const randomParentproject = uniq('apps-parent-project-');
     const parentProjectDir = 'deep/subdir';
-    const thirdProjectName = `${normalizeName(
+    const thirdParentProject = `${normalizeName(
       parentProjectDir
     )}-${randomParentproject}`;
     await runNxCommandAsync(
@@ -911,10 +1096,56 @@ describe('nx-boot-maven e2e', () => {
 
     const thirdAppName = uniq('boot-maven-app-');
     await runNxCommandAsync(
-      `generate @jnxplus/nx-boot-maven:application ${thirdAppName} --parent-project ${thirdProjectName}`
+      `generate @jnxplus/nx-boot-maven:application ${thirdAppName} --parent-project ${thirdParentProject}`
     );
     const thirdBuildResult = await runNxCommandAsync(`build ${thirdAppName}`);
     expect(thirdBuildResult.stdout).toContain('Executor ran for Build');
+
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[appsParentProject]).toContainEqual({
+      type: 'static',
+      source: appsParentProject,
+      target: parentProjectName,
+    });
+
+    expect(depGraphJson.graph.dependencies[appName]).toContainEqual({
+      type: 'static',
+      source: appName,
+      target: appsParentProject,
+    });
+
+    expect(depGraphJson.graph.dependencies[secondParentproject]).toContainEqual(
+      {
+        type: 'static',
+        source: secondParentproject,
+        target: appsParentProject,
+      }
+    );
+
+    expect(depGraphJson.graph.dependencies[secondAppName]).toContainEqual({
+      type: 'static',
+      source: secondAppName,
+      target: secondParentproject,
+    });
+
+    expect(depGraphJson.graph.dependencies[thirdParentProject]).toContainEqual({
+      type: 'static',
+      source: thirdParentProject,
+      target: secondParentproject,
+    });
+
+    expect(depGraphJson.graph.dependencies[thirdAppName]).toContainEqual({
+      type: 'static',
+      source: thirdAppName,
+      target: thirdParentProject,
+    });
   }, 1200000);
 
   it('should generate kotlin apps that use a parent project', async () => {
@@ -946,7 +1177,7 @@ describe('nx-boot-maven e2e', () => {
 
     const randomParentproject = uniq('apps-parent-project-');
     const parentProjectDir = 'deep/subdir';
-    const thirdProjectName = `${normalizeName(
+    const thirdParentProject = `${normalizeName(
       parentProjectDir
     )}-${randomParentproject}`;
     await runNxCommandAsync(
@@ -955,10 +1186,56 @@ describe('nx-boot-maven e2e', () => {
 
     const thirdAppName = uniq('boot-maven-app-');
     await runNxCommandAsync(
-      `generate @jnxplus/nx-boot-maven:application ${thirdAppName} --parent-project ${thirdProjectName} --language kotlin`
+      `generate @jnxplus/nx-boot-maven:application ${thirdAppName} --parent-project ${thirdParentProject} --language kotlin`
     );
     const thirdBuildResult = await runNxCommandAsync(`build ${thirdAppName}`);
     expect(thirdBuildResult.stdout).toContain('Executor ran for Build');
+
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[appsParentProject]).toContainEqual({
+      type: 'static',
+      source: appsParentProject,
+      target: parentProjectName,
+    });
+
+    expect(depGraphJson.graph.dependencies[appName]).toContainEqual({
+      type: 'static',
+      source: appName,
+      target: appsParentProject,
+    });
+
+    expect(depGraphJson.graph.dependencies[secondParentproject]).toContainEqual(
+      {
+        type: 'static',
+        source: secondParentproject,
+        target: appsParentProject,
+      }
+    );
+
+    expect(depGraphJson.graph.dependencies[secondAppName]).toContainEqual({
+      type: 'static',
+      source: secondAppName,
+      target: secondParentproject,
+    });
+
+    expect(depGraphJson.graph.dependencies[thirdParentProject]).toContainEqual({
+      type: 'static',
+      source: thirdParentProject,
+      target: secondParentproject,
+    });
+
+    expect(depGraphJson.graph.dependencies[thirdAppName]).toContainEqual({
+      type: 'static',
+      source: thirdAppName,
+      target: thirdParentProject,
+    });
   }, 1200000);
 
   it('should generate java libs that use a parent project', async () => {
@@ -996,7 +1273,7 @@ describe('nx-boot-maven e2e', () => {
 
     const randomParentproject = uniq('libs-parent-project-');
     const parentProjectDir = 'deep/subdir';
-    const thirdProjectName = `${normalizeName(
+    const thirdParentProject = `${normalizeName(
       parentProjectDir
     )}-${randomParentproject}`;
     await runNxCommandAsync(
@@ -1005,10 +1282,56 @@ describe('nx-boot-maven e2e', () => {
 
     const thirdLibName = uniq('boot-maven-lib-');
     await runNxCommandAsync(
-      `generate @jnxplus/nx-boot-maven:library ${thirdLibName} --parent-project ${thirdProjectName}`
+      `generate @jnxplus/nx-boot-maven:library ${thirdLibName} --parent-project ${thirdParentProject}`
     );
     const thirdBuildResult = await runNxCommandAsync(`build ${thirdLibName}`);
     expect(thirdBuildResult.stdout).toContain('Executor ran for Build');
+
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[libsParentProject]).toContainEqual({
+      type: 'static',
+      source: libsParentProject,
+      target: parentProjectName,
+    });
+
+    expect(depGraphJson.graph.dependencies[libName]).toContainEqual({
+      type: 'static',
+      source: libName,
+      target: libsParentProject,
+    });
+
+    expect(depGraphJson.graph.dependencies[secondParentproject]).toContainEqual(
+      {
+        type: 'static',
+        source: secondParentproject,
+        target: libsParentProject,
+      }
+    );
+
+    expect(depGraphJson.graph.dependencies[secondLibName]).toContainEqual({
+      type: 'static',
+      source: secondLibName,
+      target: secondParentproject,
+    });
+
+    expect(depGraphJson.graph.dependencies[thirdParentProject]).toContainEqual({
+      type: 'static',
+      source: thirdParentProject,
+      target: secondParentproject,
+    });
+
+    expect(depGraphJson.graph.dependencies[thirdLibName]).toContainEqual({
+      type: 'static',
+      source: thirdLibName,
+      target: thirdParentProject,
+    });
   }, 1200000);
 
   it('should generate kotlin libs that use a parent project', async () => {
@@ -1046,7 +1369,7 @@ describe('nx-boot-maven e2e', () => {
 
     const randomParentproject = uniq('libs-parent-project-');
     const parentProjectDir = 'deep/subdir';
-    const thirdProjectName = `${normalizeName(
+    const thirdParentProject = `${normalizeName(
       parentProjectDir
     )}-${randomParentproject}`;
     await runNxCommandAsync(
@@ -1055,9 +1378,55 @@ describe('nx-boot-maven e2e', () => {
 
     const thirdLibName = uniq('boot-maven-lib-');
     await runNxCommandAsync(
-      `generate @jnxplus/nx-boot-maven:library ${thirdLibName} --parent-project ${thirdProjectName} --language kotlin`
+      `generate @jnxplus/nx-boot-maven:library ${thirdLibName} --parent-project ${thirdParentProject} --language kotlin`
     );
     const thirdBuildResult = await runNxCommandAsync(`build ${thirdLibName}`);
     expect(thirdBuildResult.stdout).toContain('Executor ran for Build');
+
+    //graph
+    const depGraphResult = await runNxCommandAsync(
+      `dep-graph --file=dep-graph.json`
+    );
+    expect(depGraphResult.stderr).not.toContain(
+      'Failed to process the project graph'
+    );
+    const depGraphJson = readJson('dep-graph.json');
+    expect(depGraphJson.graph.dependencies[libsParentProject]).toContainEqual({
+      type: 'static',
+      source: libsParentProject,
+      target: parentProjectName,
+    });
+
+    expect(depGraphJson.graph.dependencies[libName]).toContainEqual({
+      type: 'static',
+      source: libName,
+      target: libsParentProject,
+    });
+
+    expect(depGraphJson.graph.dependencies[secondParentproject]).toContainEqual(
+      {
+        type: 'static',
+        source: secondParentproject,
+        target: libsParentProject,
+      }
+    );
+
+    expect(depGraphJson.graph.dependencies[secondLibName]).toContainEqual({
+      type: 'static',
+      source: secondLibName,
+      target: secondParentproject,
+    });
+
+    expect(depGraphJson.graph.dependencies[thirdParentProject]).toContainEqual({
+      type: 'static',
+      source: thirdParentProject,
+      target: secondParentproject,
+    });
+
+    expect(depGraphJson.graph.dependencies[thirdLibName]).toContainEqual({
+      type: 'static',
+      source: thirdLibName,
+      target: thirdParentProject,
+    });
   }, 1200000);
 });
