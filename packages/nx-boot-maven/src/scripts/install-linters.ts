25656bab65d4ff74054ef57d7c264a27bc3e6ef7
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-05-04 17:56:59 +0200

    fix libs

--- packages/nx-boot-maven/src/scripts/install-linters.ts
+++ packages/nx-boot-maven/src/scripts/install-linters.ts
@@ -1,23 +1,11 @@
-import { logger, workspaceRoot } from '@nx/devkit';
-import * as path from 'path';
-import { getCheckstyleVersion, getKtlintVersion } from '@jnxplus/maven';
 import { getCheckstylePath, getKtlintPath } from '@jnxplus/common';
+import { getCheckstyleVersion, getKtlintVersion } from '@jnxplus/maven';
+import { logger } from '@nx/devkit';
 
 (async () => {
   if (process.env.NX_VERBOSE_LOGGING === 'true') {
     logger.debug('Install Linters');
   }
-
-  const tmpProjPath = path.join(workspaceRoot, 'tmp', 'nx-e2e', 'proj');
-  const relative = path.relative(workspaceRoot, process.cwd());
-  const isSubdir =
-    relative && !relative.startsWith('..') && !path.isAbsolute(relative);
-
-  if (isSubdir) {
-    await getCheckstylePath(getCheckstyleVersion, tmpProjPath);
-    await getKtlintPath(getKtlintVersion, tmpProjPath);
-  } else {
-    await getCheckstylePath(getCheckstyleVersion);
-    await getKtlintPath(getKtlintVersion);
-  }
+  await getCheckstylePath(getCheckstyleVersion);
+  await getKtlintPath(getKtlintVersion);
 })();
