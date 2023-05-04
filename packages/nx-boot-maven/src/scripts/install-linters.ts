067e37e6c045c5ea5c9ffaa3c74181f1686f5600
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-05-04 19:59:15 +0200

    fix

--- packages/nx-boot-maven/src/scripts/install-linters.ts
+++ packages/nx-boot-maven/src/scripts/install-linters.ts
@@ -1,11 +1,39 @@
 import { getCheckstylePath, getKtlintPath } from '@jnxplus/common';
 import { getCheckstyleVersion, getKtlintVersion } from '@jnxplus/maven';
-import { logger } from '@nx/devkit';
+import { logger, workspaceRoot } from '@nx/devkit';
+import * as fs from 'fs';
+import * as path from 'path';
 
 (async () => {
   if (process.env.NX_VERBOSE_LOGGING === 'true') {
     logger.debug('Install Linters');
   }
-  await getCheckstylePath(getCheckstyleVersion);
-  await getKtlintPath(getKtlintVersion);
+
+  let workspaceRootToUse = workspaceRoot;
+  const tmpWorkspaceRoot = path.join(
+    workspaceRootToUse,
+    'tmp',
+    'nx-e2e',
+    'proj'
+  );
+
+  if (isE2eTest(tmpWorkspaceRoot)) {
+    workspaceRootToUse = tmpWorkspaceRoot;
+  }
+
+  await getCheckstylePath(getCheckstyleVersion, workspaceRootToUse);
+  await getKtlintPath(getKtlintVersion, workspaceRootToUse);
 })();
+
+function isE2eTest(tmpWorkspaceRoot: string) {
+  return (
+    fs.existsSync(tmpWorkspaceRoot) && isSubdir(tmpWorkspaceRoot, process.cwd())
+  );
+}
+
+function isSubdir(parentPath: string, childPath: string) {
+  const relative = path.relative(parentPath, childPath);
+  const isSubdir =
+    relative && !relative.startsWith('..') && !path.isAbsolute(relative);
+  return isSubdir;
+}
