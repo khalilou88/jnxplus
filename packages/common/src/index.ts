44a40c4ca9f8aa76138d4cffdb0207948363311b
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-05-06 00:20:49 +0200

    extract testing utils in a separate lib

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,5 +1,4 @@
 export * from './lib/command';
-export * from './lib/testing';
 export * from './lib/types';
 export * from './lib/utils';
 export * from './lib/versions';
---
+++ packages/e2e/testing/src/lib/testing.spec.ts
@@ -0,0 +1,5 @@
+describe('e2eTesting', () => {
+  xit('should work', () => {
+    //TODO
+  });
+});
