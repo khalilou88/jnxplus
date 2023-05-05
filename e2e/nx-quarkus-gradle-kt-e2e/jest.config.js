c712596a7042676b1eec283a57ab4d98c3726940
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-05-05 18:59:42 +0200

    add testEnvironment

--- e2e/nx-quarkus-gradle-kt-e2e/jest.config.js
+++ e2e/nx-quarkus-gradle-kt-e2e/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   displayName: 'nx-quarkus-gradle-kt-e2e',
   preset: '../../jest.preset.js',
   globals: {},
+  testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': [
       'ts-jest',
