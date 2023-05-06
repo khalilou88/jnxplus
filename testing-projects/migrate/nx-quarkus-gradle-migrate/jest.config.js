66465113183bbdc5cf84d75c7f6a949a9e1b6d14
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-05-06 17:30:57 +0200

    Testing projects folder (#180)
    * move migrate tests to testing-projects folder
    
    * move smoke tests to testing-projects folder
    
    * move e2e tests to testing-projects folder

---
+++ testing-projects/migrate/nx-quarkus-gradle-migrate/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'nx-quarkus-gradle-migrate',
+  preset: '../../../jest.preset.js',
+  globals: {},
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/migrate/nx-quarkus-gradle-migrate',
+};
---
+++ testing-projects/migrate/nx-quarkus-maven-migrate/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'nx-quarkus-maven-migrate',
+  preset: '../../../jest.preset.js',
+  globals: {},
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/migrate/nx-quarkus-maven-migrate',
+};
