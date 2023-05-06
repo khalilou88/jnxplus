f70f1392fe2ed724334e29956500b0549435b53d
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-05-06 13:33:30 +0200

    add test for nx migrate (#179)

---
+++ migrate/nx-quarkus-gradle-migrate/tests/nx-quarkus-gradle.spec.ts
@@ -0,0 +1,54 @@
+import { execSync, ExecSyncOptions } from 'child_process';
+import { join } from 'path';
+
+import { dirSync } from 'tmp';
+
+let nxOldVersion = '15.9.4';
+
+let migrateDirectory: string;
+let cleanup: () => void;
+
+const execSyncOptions: () => ExecSyncOptions = () => ({
+  cwd: join(migrateDirectory, 'test'),
+  env: {
+    ...process.env,
+    GIT_COMMITTER_NAME: 'Nx migrate Test CI',
+    GIT_COMMITTER_EMAIL: 'no-reply@fake.com',
+    GIT_AUTHOR_NAME: 'Nx migrate Test CI',
+    GIT_AUTHOR_EMAIL: 'no-reply@fake.com',
+  },
+  stdio: 'inherit',
+});
+
+describe('@jnxplus/nx-quarkus-gradle migrate', () => {
+  beforeEach(async () => {
+    ({ name: migrateDirectory, removeCallback: cleanup } = dirSync({
+      unsafeCleanup: true,
+    }));
+  });
+
+  afterEach(async () => {
+    cleanup();
+  });
+
+  it('should migrate', async () => {
+    execSync(
+      `npx create-nx-workspace@${nxOldVersion} test --preset empty --nxCloud false`,
+      {
+        cwd: migrateDirectory,
+        env: process.env,
+        stdio: 'inherit',
+      }
+    );
+
+    execSync('git init', execSyncOptions());
+
+    execSync('npm i --save-dev @jnxplus/nx-quarkus-gradle', execSyncOptions());
+
+    execSync('npx nx migrate latest', execSyncOptions());
+
+    execSync('npm i', execSyncOptions());
+
+    execSync(`git commit -am "chore: scaffold projects"`, execSyncOptions());
+  }, 1500000);
+});
