111b75a25c4dd568e14525b09773abac47f04ca6
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-05-08 00:16:57 +0200

    fix(common): fake fix to test release workflow

--- packages/common/src/lib/versions/index.ts
+++ packages/common/src/lib/versions/index.ts
@@ -3,6 +3,7 @@ export const dependencyManagementVersion = '1.1.0';
 export const checkstyleVersion = '10.10.0';
 export const ktlintVersion = '0.49.0';
 export const quarkusPlatformVersion = '2.16.6.Final';
+
 //TODO : this versions seems identical
 export const kotlinVersion = '1.7.22';
 export const kotlinJvmVersion = '1.7.22';
