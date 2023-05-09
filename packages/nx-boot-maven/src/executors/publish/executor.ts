4b9bda7e077c01914b785b78b107ae20c0829ab3
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-05-09 20:07:11 +0200

    feat(nx-boot-maven): add publish executor [ci skip] (#189)

---
+++ packages/nx-boot-maven/src/executors/publish/executor.ts
@@ -0,0 +1,14 @@
+import { ExecutorContext, logger } from '@nx/devkit';
+import { runCommand } from '@jnxplus/common';
+import { PublishExecutorSchema } from './schema';
+import { getExecutable } from '@jnxplus/maven';
+
+export default async function runExecutor(
+  options: PublishExecutorSchema,
+  context: ExecutorContext
+) {
+  logger.info(`Executor ran for Publish: ${JSON.stringify(options)}`);
+  let command = getExecutable();
+  command += ` deploy -pl :${context.projectName}`;
+  return runCommand(command);
+}
