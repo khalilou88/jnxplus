d04fed63ad9effe71cd5c49d78e72f972d6417af
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-05-16 15:50:12 +0200

    test: install @nx/devkit [skip ci]

--- testing-projects/migrate/nx-boot-gradle-migrate/tests/nx-boot-gradle.spec.ts
+++ testing-projects/migrate/nx-boot-gradle-migrate/tests/nx-boot-gradle.spec.ts
@@ -51,6 +51,8 @@ describe('@jnxplus/nx-boot-gradle migrate', () => {
 
     execSync('git init', execSyncOptions());
 
+    execSync(`npm i --save-dev @nx/devkit@latest`, execSyncOptions());
+
     execSync(
       `npm i --save-dev @jnxplus/nx-boot-gradle@latest`,
       execSyncOptions()
