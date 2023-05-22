a06df56eafaec72aaf7c086fdc51f0a5f5418f4f
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-05-22 15:51:46 +0200

    build: enable code [skip ci]

--- packages/gradle/src/lib/graph/index.ts
+++ packages/gradle/src/lib/graph/index.ts
@@ -25,11 +25,11 @@ export function addProjectsAndDependencies(
   hasher: Hasher,
   pluginName: string
 ) {
-  // if (canUseGradleTask()) {
-  addProjectsAndDependenciesFromTask(builder, hasher, pluginName);
-  // } else {
-  //   addProjectsAndDependenciesLegacy(builder, hasher, pluginName);
-  // }
+  if (canUseGradleTask()) {
+    addProjectsAndDependenciesFromTask(builder, hasher, pluginName);
+  } else {
+    addProjectsAndDependenciesLegacy(builder, hasher, pluginName);
+  }
 }
 
 function addProjectsAndDependenciesLegacy(
