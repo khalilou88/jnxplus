62e0a7414c2e86815d04fbb1ae8b7cdb6985dc80
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-06-02 21:06:13 +0200

    test: run migrate tests only if needed [skip ci]

--- testing-projects/migrate/nx-boot-maven-migrate/tests/nx-boot-maven.spec.ts
+++ testing-projects/migrate/nx-boot-maven-migrate/tests/nx-boot-maven.spec.ts
@@ -41,75 +41,59 @@ describe('@jnxplus/nx-boot-maven migrate', () => {
   });
 
   it('should migrate', async () => {
-    execSync(
-      `npx create-nx-workspace@latest test --preset apps --nxCloud false`,
-      {
-        cwd: migrateDirectory,
-        env: process.env,
-        stdio: 'inherit',
-      }
-    );
-
-    execSync('git init', execSyncOptions());
-
-    execSync(`npm i --save-dev @nx/devkit@latest`, execSyncOptions());
-
-    execSync(
-      `npm i --save-dev @jnxplus/nx-boot-maven@latest`,
-      execSyncOptions()
-    );
-
-    execSync('nx generate @jnxplus/nx-boot-maven:init', execSyncOptions());
-
-    execSync(
-      `nx g @jnxplus/nx-boot-maven:application ${testApp}`,
-      execSyncOptions()
-    );
-
-    execSync(
-      `nx g @jnxplus/nx-boot-maven:lib ${testLib} --projects ${testApp}`,
-      execSyncOptions()
-    );
-
-    execSync(
-      `nx g @jnxplus/nx-boot-maven:application ${testApp2}`,
-      execSyncOptions()
-    );
-
-    execSync(
-      `nx g @jnxplus/nx-boot-maven:application ${testApp3}`,
-      execSyncOptions()
-    );
-
-    execSync(
-      `nx g @jnxplus/nx-boot-maven:application ${testApp4}`,
-      execSyncOptions()
-    );
-
-    execSync(
-      `nx g @jnxplus/nx-boot-maven:lib ${testLib2} --projects ${testApp2},${testApp3},${testApp4}`,
-      execSyncOptions()
-    );
-
-    execSync(`nx run-many --target=build --parallel=1`, execSyncOptions());
-
-    execSync(`nx graph --file=dep-graph.json`, execSyncOptions());
-
-    const depGraphJson = await readJson(
-      join(migrateDirectory, 'test', 'dep-graph.json')
-    );
-    expect(depGraphJson.graph.nodes[testApp]).toBeDefined();
-    expect(depGraphJson.graph.nodes[testLib]).toBeDefined();
-
-    expect(depGraphJson.graph.dependencies[testApp]).toContainEqual({
-      type: 'static',
-      source: testApp,
-      target: testLib,
-    });
-
-    execSync(`git commit -am "chore: scaffold projects"`, execSyncOptions());
-
     if (ifNextVersionExists()) {
+      execSync(
+        `npx create-nx-workspace@latest test --preset apps --nxCloud false`,
+        {
+          cwd: migrateDirectory,
+          env: process.env,
+          stdio: 'inherit',
+        }
+      );
+
+      execSync('git init', execSyncOptions());
+
+      execSync(`npm i --save-dev @nx/devkit@latest`, execSyncOptions());
+
+      execSync(
+        `npm i --save-dev @jnxplus/nx-boot-maven@latest`,
+        execSyncOptions()
+      );
+
+      execSync('nx generate @jnxplus/nx-boot-maven:init', execSyncOptions());
+
+      execSync(
+        `nx g @jnxplus/nx-boot-maven:application ${testApp}`,
+        execSyncOptions()
+      );
+
+      execSync(
+        `nx g @jnxplus/nx-boot-maven:lib ${testLib} --projects ${testApp}`,
+        execSyncOptions()
+      );
+
+      execSync(
+        `nx g @jnxplus/nx-boot-maven:application ${testApp2}`,
+        execSyncOptions()
+      );
+
+      execSync(
+        `nx g @jnxplus/nx-boot-maven:application ${testApp3}`,
+        execSyncOptions()
+      );
+
+      execSync(
+        `nx g @jnxplus/nx-boot-maven:application ${testApp4}`,
+        execSyncOptions()
+      );
+
+      execSync(
+        `nx g @jnxplus/nx-boot-maven:lib ${testLib2} --projects ${testApp2},${testApp3},${testApp4}`,
+        execSyncOptions()
+      );
+
+      execSync(`git commit -am "chore: scaffold projects"`, execSyncOptions());
+
       execSync('npx nx@next migrate next', execSyncOptions());
 
       execSync('npm i --legacy-peer-deps', execSyncOptions());
@@ -120,6 +104,20 @@ describe('@jnxplus/nx-boot-maven migrate', () => {
       );
 
       execSync(`nx run-many --target=build --parallel=1`, execSyncOptions());
+
+      execSync(`nx graph --file=dep-graph.json`, execSyncOptions());
+
+      const depGraphJson = await readJson(
+        join(migrateDirectory, 'test', 'dep-graph.json')
+      );
+      expect(depGraphJson.graph.nodes[testApp]).toBeDefined();
+      expect(depGraphJson.graph.nodes[testLib]).toBeDefined();
+
+      expect(depGraphJson.graph.dependencies[testApp]).toContainEqual({
+        type: 'static',
+        source: testApp,
+        target: testLib,
+      });
 
       execSync(`git commit -am "chore: nx migrate"`, execSyncOptions());
     }
