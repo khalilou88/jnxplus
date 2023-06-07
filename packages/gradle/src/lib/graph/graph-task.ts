6a745e4582e073e8fb821476dd36f6772b9513b2
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-06-07 12:24:34 +0200

    test: gradle task

--- packages/gradle/src/lib/graph/graph-task.ts
+++ packages/gradle/src/lib/graph/graph-task.ts
@@ -108,7 +108,7 @@ function addDependencies(
       const projectName = getProjectName(project);
       const projectRoot = path.relative(workspaceRoot, project.projectDirPath);
 
-      if (projectRoot.startsWith('apps') || projectRoot.startsWith('libs')) {
+      if (!projectRoot.startsWith('apps') || !projectRoot.startsWith('libs')) {
         throw new Error(
           `ProjectRoot ${projectRoot} shoud starts with apps or libs`
         );
