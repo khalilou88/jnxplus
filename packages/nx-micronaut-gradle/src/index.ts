0cb4584613b4ee14d2a7aa9529a708e41a0aa49d
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-06-22 15:44:11 +0200

    feat(generators): export generators

--- packages/nx-micronaut-gradle/src/index.ts
+++ packages/nx-micronaut-gradle/src/index.ts
@@ -1,1 +1,13 @@
 export { processProjectGraph } from './dep-graph/lookup-deps';
+
+import initGenerator from './generators/init/generator';
+import libraryGenerator from './generators/library/generator';
+import applicationGenerator from './generators/application/generator';
+import migrateGenerator from './generators/migrate/generator';
+
+export {
+  initGenerator,
+  libraryGenerator,
+  applicationGenerator,
+  migrateGenerator,
+};
