0441b9daf316216eedac50426a5de0a9cb827804
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2023-06-23 15:18:24 +0200

    feat(executors): add keepItRunning to run-task

--- packages/gradle/src/executors/run-task/executor.ts
+++ packages/gradle/src/executors/run-task/executor.ts
@@ -1,14 +1,27 @@
 import { ExecutorContext, logger } from '@nx/devkit';
 import { getExecutable, getProjectPath } from '../../../.';
 import { RunTaskExecutorSchema } from './schema';
-import { runCommand } from '@jnxplus/common';
+import { runCommand, waitForever } from '@jnxplus/common';
 
 export default async function runExecutor(
   options: RunTaskExecutorSchema,
   context: ExecutorContext
 ) {
   logger.info(`Executor ran for Run Task: ${JSON.stringify(options)}`);
-  return runCommand(
-    `${getExecutable()} ${getProjectPath(context)}:${options.task}`
-  );
+
+  const command = `${getExecutable()} ${getProjectPath(context)}:${
+    options.task
+  }`;
+
+  const result = runCommand(command);
+
+  if (!result.success) {
+    return { success: false };
+  }
+
+  if (options.keepItRunning) {
+    await waitForever();
+  }
+
+  return { success: true };
 }
--- packages/maven/src/executors/run-task/schema.d.ts
+++ packages/maven/src/executors/run-task/schema.d.ts
@@ -1,3 +1,4 @@
 export interface RunTaskExecutorSchema {
   task: string;
+  keepItRunning: boolean;
 }
