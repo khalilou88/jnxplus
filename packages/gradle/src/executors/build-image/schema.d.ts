1d6befd01ea9b1de331a02ca7b5d40b977a89db7
khalilou88 <khalilou88@users.noreply.github.com>
2023-07-28 18:44:55 +0200

    feat(executors): remove deprecated executors

--- packages/gradle/src/executors/build-image/schema.d.ts
+++
@@ -1,7 +0,0 @@
-import { QuarkusBuildImageExecutorSchema } from '@jnxplus/common';
-import { MicronautBuildImageExecutorSchema } from './micronaut/schema';
-
-export type BuildImageExecutorSchema = MicronautBuildImageExecutorSchema &
-  QuarkusBuildImageExecutorSchema & {
-    framework?: 'spring-boot' | 'quarkus' | 'micronaut';
-  };
--- packages/maven/src/executors/build-image/schema.d.ts
+++
@@ -1,6 +0,0 @@
-import { QuarkusBuildImageExecutorSchema } from '@jnxplus/common';
-
-export type BuildImageExecutorSchema = QuarkusBuildImageExecutorSchema & {
-  framework?: 'spring-boot' | 'quarkus' | 'micronaut';
-  args: string;
-};
