38a864afc7cf6e43571ffe706e0b192dab85e3e2
khalilou88 <khalilou88@users.noreply.github.com>
2023-08-18 14:01:54 +0200

    feat(generators): update micronaut versions and generated code

--- packages/common/src/lib/versions/index.ts
+++ packages/common/src/lib/versions/index.ts
@@ -1,5 +1,5 @@
 //Kotlin
-export const kotlinVersion = '1.8.21';
+export const kotlinVersion = '1.8.22';
 
 //Spring boot
 export const springBootVersion = '3.1.0';
@@ -9,8 +9,8 @@ export const springDependencyManagementVersion = '1.1.0';
 export const quarkusVersion = '3.1.2.Final';
 
 //Micronaut
-export const micronautVersion = '4.0.0-M4';
-export const kspVersion = '1.8.21-1.0.11';
+export const micronautVersion = '4.0.4';
+export const kspVersion = '1.8.22-1.0.11';
 export const shadowVersion = '8.1.1';
 
 //Linters
