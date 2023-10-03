fae366db436fff5326551e3007bfa6faa925cc2e
khalilou88 <khalilou88@users.noreply.github.com>
2023-10-03 23:45:20 +0200

    feat(generators): add wrapper generator

---
+++ packages/nx-maven/src/generators/wrapper/generator.ts
@@ -0,0 +1,56 @@
+import { formatFiles, generateFiles, offsetFromRoot, Tree } from '@nx/devkit';
+import * as path from 'path';
+import { NxMavenWrapperGeneratorSchema } from './schema';
+
+interface NormalizedSchema extends NxMavenWrapperGeneratorSchema {
+  dot: string;
+}
+
+function normalizeOptions(
+  tree: Tree,
+  options: NxMavenWrapperGeneratorSchema,
+): NormalizedSchema {
+  const dot = '.';
+
+  return {
+    ...options,
+    dot,
+  };
+}
+
+function addFiles(tree: Tree, options: NormalizedSchema) {
+  const templateOptions = {
+    ...options,
+    offsetFromRoot: offsetFromRoot(tree.root),
+    template: '',
+  };
+  generateFiles(
+    tree,
+    path.join(__dirname, '..', 'init', 'files', 'maven', 'wrapper'),
+    '',
+    templateOptions,
+  );
+}
+
+export default async function (
+  tree: Tree,
+  options: NxMavenWrapperGeneratorSchema,
+) {
+  const normalizedOptions = normalizeOptions(tree, options);
+  addFiles(tree, normalizedOptions);
+  updateGitIgnore(tree);
+  tree.changePermissions('mvnw', '755');
+  tree.changePermissions('mvnw.cmd', '755');
+  await formatFiles(tree);
+}
+
+function updateGitIgnore(tree: Tree) {
+  const filePath = `.gitignore`;
+  const contents = tree.read(filePath, 'utf-8') || '';
+
+  const mavenIgnore =
+    '\n# Maven\ntarget/\n!.mvn/wrapper/maven-wrapper.jar\n!**/src/main/**/target/\n!**/src/test/**/target/';
+
+  const newContents = contents.concat(mavenIgnore);
+  tree.write(filePath, newContents);
+}
