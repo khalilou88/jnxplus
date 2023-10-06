8ec7f0f997c0dff0b3128da4937f25054c41a1fe
khalilou88 <khalilou88@users.noreply.github.com>
2023-10-06 17:35:07 +0200

    feat(executors): add NX_SKIP_GRADLE_WRAPPER environment variable

--- packages/common/src/lib/command/index.ts
+++ packages/common/src/lib/command/index.ts
@@ -1,5 +1,7 @@
 import { logger, workspaceRoot } from '@nx/devkit';
 import { execSync } from 'child_process';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export async function waitForever() {
   return new Promise(() => {
@@ -39,12 +41,29 @@ export function getPmdExecutable() {
 }
 
 export function getGradleExecutable() {
-  const isWin = process.platform === 'win32';
-  let executable = isWin ? 'gradlew.bat' : './gradlew';
+  let executable = '';
+
+  if (process.env['NX_SKIP_GRADLE_WRAPPER'] === 'true') {
+    executable = 'gradle';
+  } else {
+    const isWrapperExists = isWrapperExistsFunction();
+
+    if (isWrapperExists) {
+      const isWin = process.platform === 'win32';
+      executable = isWin ? 'gradlew.bat' : './gradlew';
+    } else {
+      executable = 'gradle';
+    }
+  }
 
   if (process.env['NX_GRADLE_CLI_OPTS']) {
     executable += ` ${process.env['NX_GRADLE_CLI_OPTS']}`;
   }
 
   return executable;
+}
+
+function isWrapperExistsFunction() {
+  const gradlePath = path.join(workspaceRoot, 'gradlew');
+  return fs.existsSync(gradlePath);
 }
