955c34522211989a4d83aca858583bac133ebd1b
khalilou88 <khalilou88@users.noreply.github.com>
2023-10-14 00:18:42 +0200

    build: organize imports

--- packages/common/src/executors/build-image/quarkus/executor.ts
+++ packages/common/src/executors/build-image/quarkus/executor.ts
@@ -1,5 +1,5 @@
-import { ExecutorContext, logger, workspaceRoot } from '@nx/devkit';
 import { join } from 'path';
+import { ExecutorContext, logger, workspaceRoot } from '@nx/devkit';
 import { getProjectRoot, runCommand } from '../../../.';
 import { QuarkusBuildImageExecutorSchema } from './schema';
 
