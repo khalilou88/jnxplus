6425254ec08b2cf187b2f5a533596747c6745113
khalilou88 <khalilou88@users.noreply.github.com>
2023-10-13 19:13:42 +0200

    build: add executors to update version

---
+++ packages/internal/gradle-executors/src/executors/update-xml-deps/executor.ts
@@ -0,0 +1,47 @@
+import {
+  ExecutorContext,
+  readJsonFile,
+  workspaceRoot,
+  writeJsonFile,
+} from '@nx/devkit';
+import { execSync } from 'child_process';
+import * as path from 'path';
+import { UpdateXmlDepsExecutorSchema } from './schema';
+
+export default async function runExecutor(
+  options: UpdateXmlDepsExecutorSchema,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  context: ExecutorContext,
+) {
+  console.log('Executor ran for update-xml-deps', options);
+
+  //update file
+  updateFile(options.version, 'packages/nx-maven');
+
+  const commitMessage = `release: write version ${options.version} in dependencies section`;
+
+  const commit = `git commit --no-verify -m "${commitMessage}" packages/nx-maven/package.json`;
+  execSync(commit, {
+    cwd: workspaceRoot,
+    stdio: 'inherit',
+    env: process.env,
+    encoding: 'utf-8',
+  });
+
+  const push = `git push`;
+  execSync(push, {
+    cwd: workspaceRoot,
+    stdio: 'inherit',
+    env: process.env,
+    encoding: 'utf-8',
+  });
+
+  return { success: true };
+}
+
+function updateFile(newVersion: string, projectRoot: string) {
+  const packageJsonPath = path.join(workspaceRoot, projectRoot, 'package.json');
+  const json = readJsonFile(packageJsonPath);
+  json.dependencies['@jnxplus/xml'] = `${newVersion}`;
+  writeJsonFile(packageJsonPath, json);
+}
---
+++ packages/internal/gradle-executors/src/executors/update-xml-deps/schema.d.ts
@@ -0,0 +1,3 @@
+export interface UpdateXmlDepsExecutorSchema {
+  version: string;
+}
