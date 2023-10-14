c3f1877c0436028428059f2c09d0c014a18bb75e
khalilou88 <khalilou88@users.noreply.github.com>
2023-10-14 13:46:23 +0200

    feat(plugins): add @jnxplus/nx-checkstyle plugin

---
+++ packages/nx-checkstyle/src/lib/nx-checkstyle.spec.ts
@@ -0,0 +1,7 @@
+import { nxCheckstyle } from './nx-checkstyle';
+
+describe('nxCheckstyle', () => {
+  it('should work', () => {
+    expect(nxCheckstyle()).toEqual('nx-checkstyle');
+  });
+});
---
+++ packages/nx-checkstyle/src/lib/nx-checkstyle.ts
@@ -0,0 +1,3 @@
+export function nxCheckstyle(): string {
+  return 'nx-checkstyle';
+}
