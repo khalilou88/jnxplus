eb7c87f33f0d3c0a6b487edb29692db0affd046e
khalilou88 <khalilou88@users.noreply.github.com>
2023-10-27 16:08:21 +0200

    test: refactor testing projects

---
+++ testing-projects/jnxplus-smoke-next/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'jnxplus-smoke-next',
+  preset: '../../jest.preset.js',
+  globals: {},
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/smoke-next/jnxplus-smoke-next',
+};
