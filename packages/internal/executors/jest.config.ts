bb82f4501856df7d0b9616a4df10a679bc49dd66
khalilou88 <khalilou88@users.noreply.github.com>
2023-11-12 23:23:03 +0100

    build: change project name

--- packages/internal/executors/jest.config.ts
+++ packages/internal/executors/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'internal-gradle-executors',
+  displayName: 'internal-executors',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/packages/internal/gradle-executors',
+  coverageDirectory: '../../../coverage/packages/internal/executors',
 };
