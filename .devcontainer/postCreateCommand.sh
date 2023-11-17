d3fffc4c9d2b3d8ca9d56f2965bd4abe83696fea
khalilou88 <khalilou88@users.noreply.github.com>
2023-11-17 20:17:10 +0100

    build: install Nx globally

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,5 +3,8 @@
 # Update the underlying (Debian) OS, to make sure we have the latest security patches and libraries like 'GLIBC'
 sudo apt-get update  && sudo apt-get -y upgrade
 
+#Install Nx globally
+npm install --global nx@latest
+
 # Install dependencies
 npm i
