5893dca834cab05682a7a939026a6a86b5a7dd42
khalilou88 <khalilou88@users.noreply.github.com>
2023-11-19 21:10:00 +0100

    build: work in progress

--- packages/common/src/lib/versions/index.ts
+++ packages/common/src/lib/versions/index.ts
@@ -9,7 +9,7 @@ export const springDependencyManagementVersion = '1.1.0';
 export const quarkusVersion = '3.1.2.Final';
 
 //Micronaut
-export const micronautVersion = '4.0.2';
+export const micronautVersion = '4.2.0';
 export const kspVersion = '1.8.22-1.0.11';
 export const shadowVersion = '8.1.1';
 
