505086278ab426dcbb399b1617b446439f965504
khalilou88 <khalilou88@users.noreply.github.com>
2023-12-16 22:23:28 +0100

    build: work in progress

--- packages/nx-maven/src/graph/create-nodes.ts
+++ packages/nx-maven/src/graph/create-nodes.ts
@@ -18,12 +18,24 @@ export const createNodes: CreateNodes = [
       projectName = projectJson.name;
     } else {
       const pomXmlContent = readXml(pomXmlFilePath);
+      const groupId = getGroupId(pomXmlContent);
       projectName = getArtifactId(pomXmlContent);
+      const projectVersion = getVersion(pomXmlContent);
+      const localRepositoryLocation = getLocalRepositoryLocation();
 
       targets = {
         build: {
           executor: '@jnxplus/nx-maven:run-task',
           options: {
+            outputs: [
+              `{projectRoot}/target`,
+              getOutput(
+                localRepositoryLocation,
+                groupId,
+                projectName,
+                projectVersion,
+              ),
+            ],
             task: getTask(projectRoot),
           },
         },
@@ -43,6 +55,14 @@ export const createNodes: CreateNodes = [
   },
 ];
 
+function getGroupId(pomXmlContent: XmlDocument) {
+  const groupIdXml = pomXmlContent.childNamed('groupId');
+  if (groupIdXml === undefined) {
+    throw new Error(`GroupId not found in pom.xml`);
+  }
+  return groupIdXml.val;
+}
+
 function getArtifactId(pomXmlContent: XmlDocument) {
   const artifactIdXml = pomXmlContent.childNamed('artifactId');
   if (artifactIdXml === undefined) {
@@ -51,10 +71,35 @@ function getArtifactId(pomXmlContent: XmlDocument) {
   return artifactIdXml.val;
 }
 
+function getVersion(pomXmlContent: XmlDocument) {
+  const versionXml = pomXmlContent.childNamed('version');
+  if (versionXml === undefined) {
+    throw new Error(`Version not found in pom.xml`);
+  }
+  return versionXml.val;
+}
+
+function getOutput(
+  localRepositoryLocation: string,
+  groupId: string,
+  projectName: string,
+  projectVersion: string,
+) {
+  return `${localRepositoryLocation}/.m2/repository/${groupId.replace(
+    new RegExp(/\./, 'g'),
+    '/',
+  )}/${projectName}/${projectVersion}`;
+}
+
 function getTask(projectRoot: string) {
   if (!projectRoot || projectRoot === '.') {
     return 'install -N';
   }
 
   return 'install';
+}
+
+function getLocalRepositoryLocation() {
+  const location = '{workspaceRoot}';
+  return location;
 }
