18264159ae1c70928d51fe79d99374d82731b82a
khalilou88 <khalilou88@users.noreply.github.com>
2023-12-19 16:44:13 +0100

    build: work in progress

--- testing-projects/jnxplus-e2e/tests/nx-maven/spring-boot-parent-pom.spec.ts
+++ testing-projects/jnxplus-e2e/tests/nx-maven/spring-boot-parent-pom.spec.ts
@@ -1139,7 +1139,7 @@ describe('nx-maven spring-boot-parent-pom e2e', () => {
       source: appName,
       target: libName,
     });
-  }, 120000);
+  }, 240000);
 
   it("should dep-graph don't crash when pom.xml don't contains dependencies tag", async () => {
     const libName = uniq('boot-maven-lib-');
@@ -1257,7 +1257,7 @@ describe('nx-maven spring-boot-parent-pom e2e', () => {
       source: thirdAppName,
       target: thirdParentProject,
     });
-  }, 120000);
+  }, 240000);
 
   it('should generate kotlin apps that use a parent project', async () => {
     const appsParentProject = uniq('apps-parent-project-');
