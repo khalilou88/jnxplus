789defcf8b2c85b2d383f71fceca0a2dcaed5bc9
khalilou88 <khalilou88@users.noreply.github.com>
2023-12-20 23:36:11 +0100

    test: parallel build

--- testing-projects/jnxplus-smoke/tests/nx-maven/spring-boot.spec.ts
+++ testing-projects/jnxplus-smoke/tests/nx-maven/spring-boot.spec.ts
@@ -109,7 +109,7 @@ describe('nx-maven spring-boot smoke', () => {
 
     execSync(`npx nx test ${testLib}`, execSyncOptions());
 
-    execSync(`npx nx run-many --target=build --parallel=1`, execSyncOptions());
+    execSync(`npx nx run-many --target=build --parallel`, execSyncOptions());
 
     execSync(`npx nx graph --file=dep-graph.json`, execSyncOptions());
 
