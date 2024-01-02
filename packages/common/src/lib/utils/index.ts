88043c2af3eb7055cb69a15263bf8ae175f9b70c
khalilou88 <khalilou88@users.noreply.github.com>
2024-01-02 16:21:12 +0100

    build: work in progress

--- packages/common/src/lib/utils/index.ts
+++ packages/common/src/lib/utils/index.ts
@@ -1,14 +1,4 @@
-import {
-  ExecutorContext,
-  NxJsonConfiguration,
-  readJsonFile,
-  workspaceRoot,
-} from '@nx/devkit';
-import axios from 'axios';
-import * as fs from 'fs';
-import * as path from 'path';
-import * as stream from 'stream';
-import { promisify } from 'util';
+import { ExecutorContext } from '@nx/devkit';
 import { TargetsType } from '../types';
 
 export function getProject(context: ExecutorContext) {
@@ -75,52 +65,4 @@ export function clearEmpties(o: TargetsType) {
   }
 
   return o;
-}
-
-export function getBuildTool(): '@jnxplus/nx-gradle' | '@jnxplus/nx-maven' {
-  const nxJsonPath = path.join(workspaceRoot, 'nx.json');
-
-  const nxJson = readJsonFile<NxJsonConfiguration>(nxJsonPath);
-
-  const pluginPath = '@jnxplus/nx-gradle';
-
-  const hasPlugin = nxJson.plugins?.some((p) =>
-    typeof p === 'string' ? p === pluginPath : p.plugin === pluginPath,
-  );
-
-  if (hasPlugin) {
-    return pluginPath;
-  }
-
-  return '@jnxplus/nx-maven';
-}
-
-const finished = promisify(stream.finished);
-export async function downloadFile(
-  fileUrl: string,
-  outputLocationPath: string,
-  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-): Promise<any> {
-  const writer = fs.createWriteStream(outputLocationPath);
-  return axios({
-    method: 'get',
-    url: fileUrl,
-    responseType: 'stream',
-  }).then((response) => {
-    response.data.pipe(writer);
-    return finished(writer); //this is a Promise
-  });
-}
-
-export function isE2eTest(tmpWorkspaceRoot: string) {
-  return (
-    fs.existsSync(tmpWorkspaceRoot) && isSubdir(tmpWorkspaceRoot, process.cwd())
-  );
-}
-
-function isSubdir(parentPath: string, childPath: string) {
-  const relative = path.relative(parentPath, childPath);
-  const isSubdir =
-    relative && !relative.startsWith('..') && !path.isAbsolute(relative);
-  return isSubdir;
 }
