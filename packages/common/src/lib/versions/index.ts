9e90c1518c52391986ded76a4d2ccd3dccca7ab8
khalilou88 <khalilou88@users.noreply.github.com>
2024-01-19 11:17:24 +0100

    build: update micronautCoreVersion to 4.2.3

--- packages/common/src/lib/versions/index.ts
+++ packages/common/src/lib/versions/index.ts
@@ -10,7 +10,7 @@ export const quarkusVersion = '3.5.3';
 
 //Micronaut
 export const micronautVersion = '4.2.3';
-export const micronautCoreVersion = '4.2.0';
+export const micronautCoreVersion = '4.2.3';
 export const micronautSerializationVersion = '2.4.0';
 export const micronautMavenPluginVersion = '4.0.7';
 export const micronautTestResourcesVersion = '2.2.0';
