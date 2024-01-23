3067af34ed5b7c289d0bd93adee1100df7f8dc53
khalilou88 <khalilou88@users.noreply.github.com>
2024-01-23 22:37:43 +0100

    fix: remove hyphen from scanBasePackages for kotlin projects

--- packages/internal/generators-files/spring-boot/application/shared/kotlin/src/main/kotlin/__packageDirectory__/__appClassName__.kt
+++ packages/internal/generators-files/spring-boot/application/shared/kotlin/src/main/kotlin/__packageDirectory__/__appClassName__.kt
@@ -3,7 +3,7 @@ package <%= packageName %>
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication<% if(!minimal) { %>(scanBasePackages = arrayOf("<%= groupId %>"))<% } %>
+@SpringBootApplication<% if(!minimal) { %>(scanBasePackages = arrayOf("<%= basePackage %>"))<% } %>
 class <%= appClassName %>
 
 fun main(args: Array<String>) {
