f0a2b7dcb04dc7bab53c90cde8e5be479dafc65d
khalilou88 <khalilou88@users.noreply.github.com>
2024-01-23 14:12:23 +0100

    fix: add basePackage option

--- testing-projects/jnxplus-e2e/tests/nx-gradle/spring-boot.spec.ts
+++ testing-projects/jnxplus-e2e/tests/nx-gradle/spring-boot.spec.ts
@@ -194,7 +194,7 @@ describe('nx-gradle spring-boot e2e', () => {
     const port = 8181;
 
     await runNxCommandAsync(
-      `generate @jnxplus/nx-gradle:application ${randomName} --framework spring-boot --tags e2etag,e2ePackage --directory ${appDir} --groupId com.jnxplus --projectVersion 1.2.3 --packaging war --configFormat .yml --port ${port} --simplePackageName false --simpleName false`,
+      `generate @jnxplus/nx-gradle:application ${randomName} --framework spring-boot --tags e2etag,e2ePackage --directory ${appDir} --groupId com.j-nx-plus --projectVersion 1.2.3 --packaging war --configFormat .yml --port ${port} --simplePackageName false --simpleName false`,
     );
 
     expect(() =>
