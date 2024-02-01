bd5ed13a720c854078d827d9e068a0a9e41fd3ba
khalilou88 <khalilou88@users.noreply.github.com>
2024-02-01 13:51:30 +0100

    build: work in progress

--- packages/gradle-plugin/src/main/groovy/io/github/khalilou88/jnxplus/ProjectDependencyTask.groovy
+++ packages/gradle-plugin/src/main/groovy/io/github/khalilou88/jnxplus/ProjectDependencyTask.groovy
@@ -19,7 +19,7 @@ abstract class ProjectDependencyTask extends DefaultTask {
     println("Task ran for projectDependencyTask")
     def projects = []
 
-    addProjects(projects, project, '', project)
+    addProjects(project, projects, '', project)
 
     def json_str = JsonOutput.toJson(projects)
     def json_pretty = JsonOutput.prettyPrint(json_str)
@@ -29,42 +29,40 @@ abstract class ProjectDependencyTask extends DefaultTask {
     file.write(json_pretty)
   }
 
-  def addProjects(projects, rootProject, parentProjectName, currentProject) {
+  def addProjects(rootProject, projects, parentProjectName, currentProject) {
 
-    def isBuildGradleExists      = currentProject.file('build.gradle').exists()
-    def isBuildGradleKtsExists   = currentProject.file('build.gradle.kts').exists()
+    def isBuildGradleExists = currentProject.file('build.gradle').exists()
+    def isBuildGradleKtsExists = currentProject.file('build.gradle.kts').exists()
 
-    if(isBuildGradleExists || isBuildGradleKtsExists) {
+    if (isBuildGradleExists == true || isBuildGradleKtsExists == true) {
 
-    def dependencies = currentProject.configurations
-      .findAll { it.allDependencies }
-      .collectMany { it.dependencies }
-      .findAll { it instanceof ProjectDependency }
-      .collect { element ->
-        return [
-          relativePath       : rootProject.relativePath(element.dependencyProject.projectDir),
-          name               : element.name,
-          isProjectJsonExists: element.dependencyProject.file('project.json').exists(),
-          isBuildGradleExists: element.dependencyProject.file('build.gradle').exists()]
-      }
+      def dependencies = currentProject.configurations
+        .findAll { it.allDependencies }
+        .collectMany { it.dependencies }
+        .findAll { it instanceof ProjectDependency }
+        .collect { element ->
+          return [relativePath       : rootProject.relativePath(element.dependencyProject.projectDir),
+                  name               : element.name,
+                  isProjectJsonExists: element.dependencyProject.file('project.json').exists(),
+                  isBuildGradleExists: element.dependencyProject.file('build.gradle').exists()]
+        }
 
-    projects.add([
-      relativePath             : rootProject.relativePath(currentProject.projectDir),
-      name                     : currentProject.name,
-      isProjectJsonExists      : currentProject.file('project.json').exists(),
-      isBuildGradleExists      : isBuildGradleExists,
-      isBuildGradleKtsExists   : isBuildGradleKtsExists,
-      isSettingsGradleExists   : currentProject.file('settings.gradle').exists(),
-      isSettingsGradleKtsExists: currentProject.file('settings.gradle.kts').exists(),
-      isGradlePropertiesExists : currentProject.file('gradle.properties').exists(),
-      parentProjectName        : parentProjectName,
-      dependencies             : dependencies]);
+      projects.add([relativePath             : rootProject.relativePath(currentProject.projectDir),
+                    name                     : currentProject.name,
+                    isProjectJsonExists      : currentProject.file('project.json').exists(),
+                    isBuildGradleExists      : isBuildGradleExists,
+                    isBuildGradleKtsExists   : isBuildGradleKtsExists,
+                    isSettingsGradleExists   : currentProject.file('settings.gradle').exists(),
+                    isSettingsGradleKtsExists: currentProject.file('settings.gradle.kts').exists(),
+                    isGradlePropertiesExists : currentProject.file('gradle.properties').exists(),
+                    parentProjectName        : parentProjectName,
+                    dependencies             : dependencies]);
 
     }
 
     currentProject.childProjects.each { name, childProject ->
       {
-        addProjects(projects, rootProject, currentProject.name, childProject)
+        addProjects(rootProject, projects, currentProject.name, childProject)
       }
     }
   }
