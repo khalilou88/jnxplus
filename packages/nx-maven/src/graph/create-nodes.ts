dc60d2ba24cadf823b461069b97bb5007aa56346
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-02-16 17:25:58 +0100

    feat: check if isRootProject (#886)
    Co-authored-by: khalilou88 <khalilou88@users.noreply.github.com>

--- packages/nx-maven/src/graph/create-nodes.ts
+++ packages/nx-maven/src/graph/create-nodes.ts
@@ -84,7 +84,7 @@ export const createNodes: CreateNodes<NxMavenPluginOptions> = [
             executor: '@jnxplus/nx-maven:run-task',
             outputs: outputs,
             options: {
-              task: getTask(project.projectRoot),
+              task: getTask(project.isRootProject),
               outputDirLocalRepo: outputDirLocalRepo,
             },
           },
@@ -118,8 +118,8 @@ function getOutputDirLocalRepo(
   );
 }
 
-function getTask(projectRoot: string) {
-  if (projectRoot === '.') {
+function getTask(isRootProject: boolean) {
+  if (isRootProject) {
     return 'install -N';
   }
 
