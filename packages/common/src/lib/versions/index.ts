a9ea68559376ed502477d7de420209c1401f06fc
khalilou88 <khalilou88@users.noreply.github.com>
2024-03-31 00:46:09 +0100

    build: work in progress

--- packages/common/src/lib/versions/index.ts
+++ packages/common/src/lib/versions/index.ts
@@ -23,7 +23,7 @@ export const jnxplusGradlePluginVersion = '0.4.0';
 
 //Prettier
 export const prettierVersion = '^3.2.4';
-export const prettierPluginXmlVersion = '^3.4.0';
+export const prettierPluginXmlVersion = '^3.4.1';
 export const prettierPluginJavaVersion = '2.5.0';
 
 //Maven plugins
