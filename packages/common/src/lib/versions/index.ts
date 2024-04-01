17cbb468e6a0b9709c07a4ad3bc4dc0682c64cfd
khalilou88 <khalilou88@users.noreply.github.com>
2024-04-01 17:16:25 +0200

    feat: update prettierPluginJava version

--- packages/common/src/lib/versions/index.ts
+++ packages/common/src/lib/versions/index.ts
@@ -24,7 +24,7 @@ export const jnxplusGradlePluginVersion = '0.4.0';
 //Prettier
 export const prettierVersion = '^3.2.4';
 export const prettierPluginXmlVersion = '^3.4.1';
-export const prettierPluginJavaVersion = '2.5.0';
+export const prettierPluginJavaVersion = '^2.6.0';
 
 //Maven plugins
 export const mavenCompilerPluginVersion = '3.11.0';
