96bf046b3c3b043ac098cb9f01ca6bda0f696056
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-08-01 14:32:29 +0200

    build: code optimisation

--- packages/nx-maven/src/graph/graph-utils.ts
+++ packages/nx-maven/src/graph/graph-utils.ts
@@ -64,7 +64,7 @@ export function getWorkspaceData(opts: NxMavenPluginOptions | undefined) {
 
   const skipProjectWithoutProjectJson = opts?.graphOptions
     ?.skipProjectWithoutProjectJson
-    ? opts?.graphOptions?.skipProjectWithoutProjectJson
+    ? opts.graphOptions.skipProjectWithoutProjectJson
     : false;
 
   const projects: MavenProjectType[] = [];
