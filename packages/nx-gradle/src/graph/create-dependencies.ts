564e3714c0a6463cd9a4ad1372e43eab16cb7d24
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-09-19 12:21:32 +0200

    feat: use filesToProcess to construct the nx-gradle graph

--- packages/nx-gradle/src/graph/create-dependencies.ts
+++ packages/nx-gradle/src/graph/create-dependencies.ts
@@ -2,12 +2,13 @@ import {
   CreateDependencies,
   CreateDependenciesContext,
   DependencyType,
-  RawProjectGraphDependency,
   joinPathFragments,
+  RawProjectGraphDependency,
   validateDependency,
 } from '@nx/devkit';
+import * as path from 'path';
 import { getGradleRootDirectory } from '../utils';
-import { getGradleProjects } from './graph-utils';
+import { getGradleProjects, GradleProjectType } from './graph-utils';
 
 export const createDependencies: CreateDependencies = (
   _,
@@ -19,41 +20,69 @@ export const createDependencies: CreateDependencies = (
 
   const projects = getGradleProjects();
 
-  for (const project of projects) {
-    const buildFile = project.isBuildGradleExists
-      ? 'build.gradle'
-      : 'build.gradle.kts';
+  Object.keys(context.filesToProcess.projectFileMap).forEach((source) => {
+    Object.values(context.filesToProcess.projectFileMap[source]).forEach(
+      (fileData) => {
+        const filePath = fileData.file;
+        if (
+          path.basename(filePath) === 'build.gradle' ||
+          path.basename(filePath) === 'build.gradle.kts'
+        ) {
+          const project = projects.find(
+            (element: GradleProjectType) =>
+              joinPathFragments(
+                gradleRootDirectory,
+                element.relativePath,
+                'build.gradle',
+              ) === filePath ||
+              joinPathFragments(
+                gradleRootDirectory,
+                element.relativePath,
+                'build.gradle.kts',
+              ) === filePath,
+          );
 
-    const projectSourceFile = joinPathFragments(
-      gradleRootDirectory,
-      project.relativePath,
-      buildFile,
+          if (!project) {
+            throw new Error(`Can't find project for file: ${filePath}`);
+          }
+
+          const buildFile = project.isBuildGradleExists
+            ? 'build.gradle'
+            : 'build.gradle.kts';
+
+          const projectSourceFile = joinPathFragments(
+            gradleRootDirectory,
+            project.relativePath,
+            buildFile,
+          );
+
+          if (project.parentProjectName) {
+            const newDependency = {
+              source: project.name,
+              target: project.parentProjectName,
+              sourceFile: projectSourceFile,
+              type: DependencyType.static,
+            };
+
+            validateDependency(newDependency, context);
+            results.push(newDependency);
+          }
+
+          for (const dependency of project.dependencies) {
+            const newDependency = {
+              source: project.name,
+              target: dependency.name,
+              sourceFile: projectSourceFile,
+              type: DependencyType.static,
+            };
+
+            validateDependency(newDependency, context);
+            results.push(newDependency);
+          }
+        }
+      },
     );
-
-    if (project.parentProjectName) {
-      const newDependency = {
-        source: project.name,
-        target: project.parentProjectName,
-        sourceFile: projectSourceFile,
-        type: DependencyType.static,
-      };
-
-      validateDependency(newDependency, context);
-      results.push(newDependency);
-    }
-
-    for (const dependency of project.dependencies) {
-      const newDependency = {
-        source: project.name,
-        target: dependency.name,
-        sourceFile: projectSourceFile,
-        type: DependencyType.static,
-      };
-
-      validateDependency(newDependency, context);
-      results.push(newDependency);
-    }
-  }
+  });
 
   return results;
 };
