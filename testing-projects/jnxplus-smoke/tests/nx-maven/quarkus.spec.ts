875727ac90da35800c0394feb7e8c2f7f63833e5
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-10-15 16:13:54 +0200

    test: use ts preset

--- testing-projects/jnxplus-smoke/tests/nx-maven/quarkus.spec.ts
+++ testing-projects/jnxplus-smoke/tests/nx-maven/quarkus.spec.ts
@@ -48,7 +48,7 @@ describe('nx-maven quarkus smoke', () => {
 
   it('should work', async () => {
     execSync(
-      `npx create-nx-workspace@${process.env.NX_NPM_TAG} test --preset apps --nxCloud skip`,
+      `npx create-nx-workspace@${process.env.NX_NPM_TAG} test --preset ts --nxCloud skip`,
       {
         cwd: smokeDirectory,
         env: process.env,
